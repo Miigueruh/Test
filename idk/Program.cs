f8fd25860a8f98df6e3c97a15e84481459f51fa9
Miguel <miguelhenriqueblox@gmail.com>
2023-07-27 09:10:23 -0400

    first

--- idk/Program.cs
+++ idk/Program.cs
@@ -3,6 +3,7 @@ class idk
 {
     static void Main(string[] args)
     {
+        /*
         int[] n1 = new int[5];
         //int[] n2 = new int[5] {0, 3, 6, 7, 6 };
 
@@ -14,7 +15,7 @@ class idk
         n1[1] = Convert.ToInt32(Console.ReadLine());
         n1[2] = (n1[0] + n1[1]) / 2;
         Console.WriteLine(n1[2]);
-
+        */
 
         List<string> lista = new List<string>();
         lista.Add("Nome");
@@ -23,9 +24,14 @@ class idk
         //lista.Sort();
         lista.Insert(0, "Nombre");
         lista.Remove("Nome");
-        //lista.RemoveAt(0);
+        //lista.RemoveAt(0); //Remove uma unidade em uma posição
+        //lista.Clear(); //Limpa a lista
+        //lista.Reverse(); //Reverte a lista
 
+        //Console.WriteLine(lista.Count); //Conta o tamanho da lista
+        //Console.WriteLine(lista.Contains("Menguel")); //Verifica se possui ou não um dado específico, retornando True ou False
 
+        //Console.WriteLine(lista.IndexOf("Menguel"));
 
         foreach (string s in lista)
         {
